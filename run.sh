978db05926dd79fea9b43c4622f44234c49fb475
Yoshihiro OKUMURA <orrisroot@gmail.com>
2022-05-10 09:34:43 +0900

    add support to build ubuntu-18.04 and ubuntu-20.04 packages.

--- run.sh
+++ run.sh
@@ -3,7 +3,7 @@
 usage () {
   echo "Usage: $(basename $1) TYPE"
   echo "options:"
-  echo "  - TYPE: centos-7 alma-8 rocky-8 centos-stream-8"
+  echo "  - TYPE: centos-7 almalinux-8 rocky-8 centos-stream-8 ubuntu-18.04 ubuntu-20.04"
 }
 if [ $# -ne 1 ]; then
   usage $0
@@ -11,7 +11,7 @@ if [ $# -ne 1 ]; then
 fi
 
 case $1 in
-  cent*-7)
+  centos-7)
     docker run --cap-add SYS_ADMIN -it --rm --name c7 -v $(pwd)/work:/work docker.io/library/centos:7 /bin/bash /work/build.sh
     ;;
   alma*-8)
@@ -20,8 +20,14 @@ case $1 in
   rocky*-8)
     docker run --cap-add SYS_ADMIN -it --rm --name r8 -v $(pwd)/work:/work docker.io/library/rockylinux:8 /bin/bash /work/build.sh
     ;;
-  cent*-stream-8)
+  centos-stream-8)
     docker run --cap-add SYS_ADMIN -it --rm --name cs8 -v $(pwd)/work:/work quay.io/centos/centos:stream8 /bin/bash /work/build.sh
+    ;;
+  ubuntu-18.04)
+    docker run --cap-add SYS_ADMIN -it --rm --name u1804 -v $(pwd)/work:/work docker.io/library/ubuntu:18.04 /bin/bash /work/build.sh
+    ;;
+  ubuntu-20.04)
+    docker run --cap-add SYS_ADMIN -it --rm --name u2004 -v $(pwd)/work:/work docker.io/library/ubuntu:20.04 /bin/bash /work/build.sh
     ;;
   *)
     echo "Error: unknown type: $1"
--- work/build.sh
+++ work/build.sh
@@ -5,89 +5,28 @@ if [ ! -f /.dockerenv ]; then
   exit 1
 fi
 
-if [ ! -f /etc/system-release-cpe ]; then
-  echo "Error: /etc/system-release-cpe file not found"
+if [ ! -f /etc/os-release ]; then
+  echo "Error: /etc/os-release file not found"
   exit 1
 fi
 
-in_array() {
-  for item in "${@:2}"; do [[ "$item" == "$1" ]] && return 0; done
-  return 1
-}
-
-SYSTEM_CPE=$(cat /etc/system-release-cpe)
-# cpe:/o:centos:centos:7
-# cpe:/o:centos:centos:8
-# cpe:/o:rocky:rocky:8:GA
-# cpe:/o:rocky:rocky:8.5:GA
-# cpe:/o:almalinux:almalinux:8::baseos
-# cpe:/o:fedoraproject:fedora:35
-
-SYSTEM_PRODUCT=$(echo $SYSTEM_CPE | awk -F : '{ print $4 }')
-SYSTEM_VERSION=$(echo $SYSTEM_CPE | awk -F : '{ print $5 }')
-
-SUPPORTED_SYSTEM_PRODUCTS=(centos rocky almalinux)
-in_array ${SYSTEM_PRODUCT} ${SUPPORTED_SYSTEM_PRODUCTS[@]}
-if [ $? -ne 0 ]; then
-  echo "Error: unsupported system product found: ${SYSTEM_PRODUCT}"
-  exit 1
-fi
+. /etc/os-release
+SYSTEM_ID="${ID}-${VERSION_ID}"
 
 pushd $(dirname $0)
 WORK_DIR="$(pwd)"
 popd
 LUSTRE_SOURCE="$(ls ${WORK_DIR}/*.tgz 2> /dev/null)"
 
-case "${SYSTEM_VERSION}" in
-  8*)
-    dnf install -y dnf-plugins-core
-    dnf config-manager --enable powertools
-    dnf install -y kernel-devel kernel-headers
-    dnf install -y rpm-build libtool make elfutils-libelf-devel
-    # dnf install -y which kernel-rpm-macros kernel-abi-stablelists kmod
-    dnf install -y krb5-devel openssl-devel libyaml-devel
-
-    KERNEL_VERSION=$(rpm -qa kernel-devel | sed -e 's+kernel-devel-++')
-    DEST_DIR="${WORK_DIR}/dist/${SYSTEM_PRODUCT}-${SYSTEM_VERSION}/${KERNEL_VERSION}"
-    [ ! -d ${DEST_DIR} ] && mkdir -p ${DEST_DIR}
-    cd ${DEST_DIR}
-
-    if [ -f "${LUSTRE_SOURCE}" ]; then
-      tar zxvf "${LUSTRE_SOURCE}"
-      pushd $(basename "${LUSTRE_SOURCE}" .tgz)
-    else
-      dnf install -y git
-      git clone --branch b2_12 git://git.whamcloud.com/fs/lustre-release.git
-      pushd lustre-release
-    fi
-    sh ./autogen.sh
-    ./configure --disable-server --disable-lru-resize --with-linux=/usr/src/kernels/${KERNEL_VERSION}
-    make srpm
-    SOURCE_RPM="$(pwd)/$(ls *src.rpm)"
-    popd
-
-    dnf install -y epel-release
-    dnf install -y mock
-    if [ "${SYSTEM_PRODUCT}" = "centos" ]; then 
-      MOCK_CONFIG=centos-stream-8-x86_64
-    else
-      MOCK_CONFIG=${SYSTEM_PRODUCT}-8-x86_64
-    fi
-    mock -r ${MOCK_CONFIG} init
-    mock -r ${MOCK_CONFIG} install kernel-devel-${KERNEL_VERSION} kernel-headers-${KERNEL_VERSION} kernel-abi-stablelists
-    mock -r ${MOCK_CONFIG} install krb5-devel openssl-devel
-    mock -r ${MOCK_CONFIG} --no-clean --define "configure_args '--disable-lru-resize'" --define "kdir /usr/src/kernels/${KERNEL_VERSION}" --define "kobjdir /usr/src/kernels/${KERNEL_VERSION}" --without servers --without ldiskfs --with gss_keyring --with gss --with gss --without snmp ${SOURCE_RPM}
-    cp /var/lib/mock/${MOCK_CONFIG}/result/*.rpm ${DEST_DIR}/
-    cp /var/lib/mock/${MOCK_CONFIG}/result/build.log ${DEST_DIR}/
-    ;;
-
-  7*)
+case "${SYSTEM_ID}" in
+  centos-7*)
     yum install -y kernel-devel kernel-headers
     yum install -y rpm-build libtool make elfutils-libelf-devel
     yum install -y krb5-devel openssl-devel libyaml-devel
 
-    KERNEL_VERSION=$(rpm -qa kernel-devel | sed -e 's+kernel-devel-++')
-    DEST_DIR="${WORK_DIR}/dist/${SYSTEM_PRODUCT}-${SYSTEM_VERSION}/${KERNEL_VERSION}"
+    KERNEL_VERSION="$(rpm -qa kernel-devel | sed -e 's+kernel-devel-++')"
+    KERNEL_ARCH="$(echo ${KERNEL_VERSION} | sed -e 's+.*\.++g')"
+    DEST_DIR="${WORK_DIR}/dist/${SYSTEM_ID}/${KERNEL_VERSION}"
     [ ! -d ${DEST_DIR} ] && mkdir -p ${DEST_DIR}
     cd ${DEST_DIR}
 
@@ -107,7 +46,7 @@ case "${SYSTEM_VERSION}" in
 
     yum install -y epel-release
     yum install -y mock
-    MOCK_CONFIG=${SYSTEM_PRODUCT}-7-x86_64
+    MOCK_CONFIG="$(echo $SYSTEM_ID | sed -e 's+7\..*$+7+g')-$KERNEL_ARCH"
     mock -r ${MOCK_CONFIG} init
     mock -r ${MOCK_CONFIG} install kernel-devel-${KERNEL_VERSION} kernel-headers-${KERNEL_VERSION} kernel-abi-whitelists
     mock -r ${MOCK_CONFIG} install krb5-devel openssl-devel
@@ -116,8 +55,75 @@ case "${SYSTEM_VERSION}" in
     cp /var/lib/mock/${MOCK_CONFIG}/result/build.log ${DEST_DIR}/
     ;;
 
+  centos-8*|rocky-8*|almalinux-8*)
+    dnf install -y dnf-plugins-core
+    dnf config-manager --enable powertools
+    dnf install -y kernel-devel kernel-headers
+    dnf install -y rpm-build libtool make elfutils-libelf-devel
+    # dnf install -y which kernel-rpm-macros kernel-abi-stablelists kmod
+    dnf install -y krb5-devel openssl-devel libyaml-devel
+
+    KERNEL_VERSION="$(rpm -qa kernel-devel | sed -e 's+kernel-devel-++')"
+    KERNEL_ARCH="$(echo ${KERNEL_VERSION} | sed -e 's+.*\.++g')"
+    DEST_DIR="${WORK_DIR}/dist/${SYSTEM_ID}/${KERNEL_VERSION}"
+    [ ! -d ${DEST_DIR} ] && mkdir -p ${DEST_DIR}
+    cd ${DEST_DIR}
+
+    if [ -f "${LUSTRE_SOURCE}" ]; then
+      tar zxvf "${LUSTRE_SOURCE}"
+      pushd $(basename "${LUSTRE_SOURCE}" .tgz)
+    else
+      dnf install -y git
+      git clone --branch b2_12 git://git.whamcloud.com/fs/lustre-release.git
+      pushd lustre-release
+    fi
+    sh ./autogen.sh
+    ./configure --disable-server --disable-lru-resize --with-linux=/usr/src/kernels/${KERNEL_VERSION}
+    make srpm
+    SOURCE_RPM="$(pwd)/$(ls *src.rpm)"
+    popd
+
+    dnf install -y epel-release
+    dnf install -y mock
+    MOCK_CONFIG="$(echo $SYSTEM_ID | sed -e 's+centos+centos-stream+g' -e 's+8\..*$+8+g')-$KERNEL_ARCH"
+    mock -r ${MOCK_CONFIG} init
+    mock -r ${MOCK_CONFIG} install kernel-devel-${KERNEL_VERSION} kernel-headers-${KERNEL_VERSION} kernel-abi-stablelists
+    mock -r ${MOCK_CONFIG} install krb5-devel openssl-devel
+    mock -r ${MOCK_CONFIG} --no-clean --define "configure_args '--disable-lru-resize'" --define "kdir /usr/src/kernels/${KERNEL_VERSION}" --define "kobjdir /usr/src/kernels/${KERNEL_VERSION}" --without servers --without ldiskfs --with gss_keyring --with gss --with gss --without snmp ${SOURCE_RPM}
+    cp /var/lib/mock/${MOCK_CONFIG}/result/*.rpm ${DEST_DIR}/
+    cp /var/lib/mock/${MOCK_CONFIG}/result/build.log ${DEST_DIR}/
+    ;;
+
+  ubuntu-18.04|ubuntu-20.04)
+    export DEBIAN_FRONTEND=noninteractive
+    apt update
+    apt install -y linux-headers-generic
+    apt install -y build-essential libyaml-dev module-assistant libreadline-dev debhelper dpatch libselinux-dev libsnmp-dev mpi-default-dev quilt rsync
+    apt install -y libkrb5-dev libssl-dev libgss-dev libkeyutils-dev zlib1g-dev
+
+    KERNEL_VERSION="$(ls -d /usr/src/linux-headers-*-generic | sed -e 's+.*/linux-headers-++g')"
+    DEST_DIR="${WORK_DIR}/dist/${SYSTEM_ID}/${KERNEL_VERSION}"
+    [ ! -d ${DEST_DIR} ] && mkdir -p ${DEST_DIR}
+    cd ${DEST_DIR}
+
+    if [ -f "${LUSTRE_SOURCE}" ]; then
+      tar zxvf "${LUSTRE_SOURCE}"
+      pushd $(basename "${LUSTRE_SOURCE}" .tgz)
+    else
+      apt install -y git
+      git clone --branch b2_12 git://git.whamcloud.com/fs/lustre-release.git
+      pushd lustre-release
+    fi
+    sh ./autogen.sh
+    ./configure --disable-server --disable-lru-resize --with-linux="/usr/src/linux-headers-${KERNEL_VERSION}/"
+    make debs | tee build.log
+    cp ./debs/*.deb ${DEST_DIR}/
+    cp ./build.log ${DEST_DIR}/
+    popd
+    ;;
+
   *)
-    echo "Error: unsupported system version found: ${SYSTEM_VERSION}"
+    echo "Error: unsupported system version found: ${SYSTEM_ID}"
     exit 1
     ;;
 esac
